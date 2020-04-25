d8a1408700ae1552d116e4a81455f9eed3dbcb4a
hans sanchez <64324668+hansitoSanchez@users.noreply.github.com>
2020-04-25 19:32:23 +0200

    test JUnit

---
+++ ClaseUnoJUnit.java
@@ -0,0 +1,15 @@
+package actividades;
+
+public class ClaseUnoJUnit {
+	// Metodo que siempre devuelve true
+	public boolean DevuelveTrue()
+	{
+		return true;
+	}
+
+	// Metodo que devuelve el valor entero recibido como parametro
+	public int DevuelveEnt(String sPrueba, int iValEnt)
+	{
+		return iValEnt;
+	}
+}
